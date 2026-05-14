af7a73a55f0fca77dbe4367eaafa06f9d8080397
Maryam Inriyanti <maryam24@mhs.unitama.ac.id>
2026-05-14 21:34:32 +0800

    feat: pembuatan migration, seeder, dan factory untuk Petugas

---
+++ database/factories/PetugasFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Petugas;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Petugas>
+ */
+class PetugasFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nama_petugas' => fake()->name(),
+            'nip' => fake()->numerify('##################'),
+            'alamat' => fake()->address(),
+            'no_hp' => fake()->phoneNumber(),
+            'email' => fake()->unique()->safeEmail(),
+        ];
+    }
+}
